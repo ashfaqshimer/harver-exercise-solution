c53a0b5b0f7413b45184cb3a461dc7512185d17f
Shimer <ashfaq.shimer@itelasoft.com>
2019-11-21 10:23:50 +0530

    Complete task 1

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+function printWords() {
+  for (let i = 0; i <= 100; i++) {
+    console.log(`${i} : ${getRandomWordSync()}`);
+  }
+}
+
+const rando = printWords();
+
+console.log(rando);
