d32ef5aa2c553dee33dfda28b514c61ff61e7482
Shimer <ashfaq.shimer@itelasoft.com>
2019-11-21 10:56:32 +0530

    Complete task 2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+// Task 1 - Random Words
 function printWords() {
   for (let i = 0; i <= 100; i++) {
-    console.log(`${i} : ${getRandomWordSync()}`);
+    console.log(`${i} : ${getRandomWordSync({ withErrors: false })}`);
   }
 }
 
-const rando = printWords();
+// Task 2 - FizzBuzz
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    let word = getRandomWordSync();
+
+    const fizz = i % 3 === 0;
+    const buzz = i % 5 === 0;
+
+    if (fizz && buzz) {
+      word = 'FizzBuzz';
+    } else if (fizz) {
+      word = 'Fizz';
+    } else if (buzz) {
+      word = 'Buzz';
+    }
+
+    console.log(`${i} : ${word}`);
+  }
+}
+
+const rando = fizzBuzz();
 
 console.log(rando);
