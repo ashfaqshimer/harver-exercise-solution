b6d6447ca666835e386f9f3ea1cf182397bcde4b
Shimer <ashfaq.shimer@itelasoft.com>
2019-11-21 11:29:56 +0530

    Complete task 5

--- src/index.js
+++ src/index.js
@@ -65,4 +65,39 @@ async function asyncFizzBuzz() {
   }
 }
 
-const rando = asyncFizzBuzz();
+// Task 4 - Display an error
+async function asyncErrorPrintWords() {
+  for (let i = 1; i <= 100; i++) {
+    try {
+      const word = await getRandomWord({ withErrors: true });
+      console.log(`${i} : ${word}`);
+    } catch (error) {
+      console.log(`${i} : It shouldn't break anything`);
+    }
+  }
+}
+
+async function asyncErrorFizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    try {
+      let word = await getRandomWord({ withErrors: true });
+
+      const fizz = i % 3 === 0;
+      const buzz = i % 5 === 0;
+
+      if (fizz && buzz) {
+        word = 'FizzBuzz';
+      } else if (fizz) {
+        word = 'Fizz';
+      } else if (buzz) {
+        word = 'Buzz';
+      }
+
+      console.log(`${i} : ${word}`);
+    } catch (error) {
+      console.log(`${i} : It shouldn't break anything!`);
+    }
+  }
+}
+
+const rando = asyncErrorFizzBuzz();
